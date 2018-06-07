e797d2bc98e40402831e29756ac61a6a0e31e990
Yuki Finamore <yuki@loldesign.com.br>
2018-06-06 22:05:31 -0300

    Added images general

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,7 +1,6 @@
 class LoginController < ApplicationController
   def index
   	resource = Teacher.find_by_email params[:username]
-  		binding.pry
   	if resource&.valid_password? params[:password]
   		sign_in('teacher',resource)
   		redirect_to register_path
