69ab25951fe46dfe940689a00e6dacd1a6aaf40f
Diego Zago <diego@loldesign.com.br>
2018-06-08 04:43:05 -0300

    Added question background images and start front of questions pages

--- config/routes.rb
+++ config/routes.rb
@@ -16,18 +16,18 @@ Rails.application.routes.draw do
     end
   end
 
-  get ':student_id/questions-1'  => 'questions#one'
-  get ':student_id/questions-2'  => 'questions#two'
-  get ':student_id/questions-3'  => 'questions#three'
-  get ':student_id/questions-4'  => 'questions#four'
-  get ':student_id/questions-5'  => 'questions#five'
-  get ':student_id/questions-6'  => 'questions#six'
-  get ':student_id/questions-7'  => 'questions#seven'
-  get ':student_id/questions-8'  => 'questions#eight'
-  get ':student_id/questions-9'  => 'questions#nine'
-  get ':student_id/questions-10' => 'questions#ten'
-  get ':student_id/questions-11' => 'questions#eleven'
-  get ':student_id/send_question' => 'questions#send_question'
+  get '/student/:student_id/questions-1'  => 'questions#one'
+  get '/student/:student_id/questions-2'  => 'questions#two'
+  get '/student/:student_id/questions-3'  => 'questions#three'
+  get '/student/:student_id/questions-4'  => 'questions#four'
+  get '/student/:student_id/questions-5'  => 'questions#five'
+  get '/student/:student_id/questions-6'  => 'questions#six'
+  get '/student/:student_id/questions-7'  => 'questions#seven'
+  get '/student/:student_id/questions-8'  => 'questions#eight'
+  get '/student/:student_id/questions-9'  => 'questions#nine'
+  get '/student/:student_id/questions-10' => 'questions#ten'
+  get '/student/:student_id/questions-11' => 'questions#eleven'
+  get '/student/:student_id/send_question' => 'questions#send_question'
 
   namespace :api do
   	namespace :v1 do
