d2864d3612289df84f0d56f44550c0a3494060a7
Diego Zago <diego@loldesign.com.br>
2018-06-08 03:03:44 -0300

    Adjusted styles of students-results

--- app/controllers/teacher/teacher_login_controller.rb
+++ app/controllers/teacher/teacher_login_controller.rb
@@ -1,7 +1,7 @@
 class Teacher::TeacherLoginController < ApplicationController
 	def index
 		resource = Teacher.find_by_email params[:username]
-		if resource&.valid_password? params[:password]
+		if resource.present? && resource.valid_password?(params[:password])
 			sign_in('teacher',resource)
 			redirect_to professor_students_results_path(resource.id)
 		end
